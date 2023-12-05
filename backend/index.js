e5d3181cc69f1faacd2df944123f05a00a5240b5
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-05 21:21:55 +0530

    Create First HTTP request

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT } from "./config.js";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to MERN Stack Tutorial");
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
