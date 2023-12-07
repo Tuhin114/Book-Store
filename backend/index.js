bb92622cfb4bf12d76cf478d284f9c549a0ac9aa
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-07 12:43:25 +0530

    CORS policy

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,26 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
 
+//Middleware for handling CORS POLICY
+
+//Option 1: Allow All Origins with default of cors(*)
+app.use(cors());
+
+//Option 2: Allow Custom Origins
+app.use(
+  cors({
+    origin: "http://localhost:3000", // add your domain here if you want
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Welcome to MERN Stack Tutorial");
