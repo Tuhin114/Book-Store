b28a98890ad7f0cdba42d8af5921b4aedf75d51c
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-11 11:18:19 +0530

    Alert function

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,36 +1,44 @@
-import {  useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import Spinner from '../components/spinner';
 import BackButton from '../components/BackButton';
-import { useNavigate,useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
 const DeleteBook = () => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar(); // Corrected the function name here
   const { id } = useParams();
-  const handleDeleteBook = () =>{
-    setLoading(false);
+
+  const handleDeleteBook = () => {
+    setLoading(true); // Set loading to true before the API call
     axios.delete(`http://localhost:5555/books/${id}`)
-    .then(()=>{
-      setLoading(false);
-      navigate('/');
-    }).catch((error)=>{
-      setLoading(false);
-      alert("An error happend. Please Check console");
-      console.log(error);
-    })
-  }
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar('Book Deleted Successfully', { variant: 'success' });
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        enqueueSnackbar('Error Deleting Book', { variant: 'error' });
+        console.log(error);
+      });
+  };
+
   return (
     <div className='p-4'>
       <BackButton />
       <h1 className='text-3xl my-4'>Delete Book</h1>
-      {loading ? <Spinner/> : ''}
+      {loading ? <Spinner /> : ''}
       <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
-        <h3 className='text-2x1'>Are You Sure You want to Delete this book?</h3>
-        <button className='p-4 bg-red-600 text-white m-8 w-full' onClick={handleDeleteBook}>Yes,I Delete it</button>
-    </div>  
+        <h3 className='text-2xl'>Are You Sure You want to Delete this book?</h3>
+        <button className='p-4 bg-red-600 text-white m-8 w-full' onClick={handleDeleteBook}>
+          Yes, I Delete it
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DeleteBook
+export default DeleteBook;
