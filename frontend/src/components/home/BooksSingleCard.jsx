c6711677944551d36c51e74fb11da5b18e301fc9
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-11 09:05:45 +0530

    Books card in single component

---
+++ frontend/src/components/home/BooksSingleCard.jsx
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom';
+import { PiBookOpenTextLight } from 'react-icons/pi';
+import { BiUserCircle, BiPencil, BiTrash } from 'react-icons/bi'; // Import BiPencil and BiTrash icons
+import { BsInfoCircle } from 'react-icons/bs';
+import PropTypes from 'prop-types'; // Import PropTypes
+
+const BooksSingleCard = ({ book }) => {
+  return (
+    <div key={book._id} className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'>
+      <h2 className='absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg'>{book.publishYear}</h2>
+      <h4 className='my-2 text-gray-500'>{book._id}</h4>
+      <div className='flex justify-start items-center gap-x-2'>
+        <PiBookOpenTextLight className='text-red-300 text-2xl' />
+        <h2 className='my-1'>{book.title}</h2>
+      </div>
+      <div className='flex justify-start items-center gap-x-2'>
+        <BiUserCircle className='text-red-300 text-2xl' />
+        <h2 className='my-1'>{book.author}</h2>
+      </div>
+      <div className='flex justify-start items-center gap-x-2 mt-4 p-4'>
+        <Link to={`/books/details/${book._id}`}>
+          <BsInfoCircle className='text-2xl text-green-800 hover:text-black' />
+        </Link>
+        <Link to={`/books/edit/${book._id}`}>
+          <BiPencil className='text-2xl text-yellow-800 hover:text-black' />
+        </Link>
+        <Link to={`/books/delete/${book._id}`}>
+          <BiTrash className='text-2xl text-red-800 hover:text-black' />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+BooksSingleCard.propTypes = {
+  book: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    publishYear: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    // Add more PropTypes for other properties if necessary
+  }).isRequired,
+};
+
+export default BooksSingleCard;
