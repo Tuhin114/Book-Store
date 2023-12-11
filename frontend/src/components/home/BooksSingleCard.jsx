40f7fa8c4ca188f78f2599a8d67177ea3672a69b
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-11 09:53:20 +0530

    Book Modal function

--- frontend/src/components/home/BooksSingleCard.jsx
+++ frontend/src/components/home/BooksSingleCard.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
 import { PiBookOpenTextLight } from 'react-icons/pi';
-import { BiUserCircle, BiPencil, BiTrash } from 'react-icons/bi'; // Import BiPencil and BiTrash icons
+import { BiUserCircle, BiPencil, BiTrash, BiShow } from 'react-icons/bi'; // Import BiPencil and BiTrash icons
 import { BsInfoCircle } from 'react-icons/bs';
+import { useState } from 'react';
+import BookModal from './BookModal';
 import PropTypes from 'prop-types'; // Import PropTypes
 
 const BooksSingleCard = ({ book }) => {
+    const [showModal, setShowModal] = useState(false);
   return (
     <div key={book._id} className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'>
       <h2 className='absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg'>{book.publishYear}</h2>
@@ -18,6 +21,8 @@ const BooksSingleCard = ({ book }) => {
         <h2 className='my-1'>{book.author}</h2>
       </div>
       <div className='flex justify-start items-center gap-x-2 mt-4 p-4'>
+        <BiShow className='text-3xl text-blue-800 hover:text-black cursor-pointer'
+        onClick={()=> setShowModal(true)}/>
         <Link to={`/books/details/${book._id}`}>
           <BsInfoCircle className='text-2xl text-green-800 hover:text-black' />
         </Link>
@@ -28,6 +33,11 @@ const BooksSingleCard = ({ book }) => {
           <BiTrash className='text-2xl text-red-800 hover:text-black' />
         </Link>
       </div>
+      {
+        showModal && (
+            <BookModal book={book} onClose={()=> setShowModal(false)}/>
+        )
+      }
     </div>
   )
 }
